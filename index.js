caa1e6565f57df7b26a332eafc8e45262b2b3e6d
Nafis Garifullin <garifullinn@gmail.com>
2024-03-26 19:26:33 +0500

    bored-api

---
+++ index.js
@@ -0,0 +1,20 @@
+// const inner = document.querySelector(".js_inner");
+// const innerHeader = document.querySelector(".js-inner-header");
+// const innerText = document.querySelector(".js-inner-text");
+// const btnGo = document.querySelector(".js-btn-go");
+
+// btnGo.addEventListener("click", getActivity);
+
+// function getActivity() {
+//   fetch("http://www.boredapi.com/api/activity/")
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((value) => {
+//       inner.classList.add("inner_green");
+//       innerHeader.innerText = "Ура, теперь не скучно 🔥";
+//       innerText.innerText = value.activity;
+//       console.log(value.activity);
+//     });
+// }
+
